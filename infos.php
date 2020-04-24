1415cb5e9f1d678bed519151e774959acf5724fb
Jon-Akademy <j.peraudon@it-students.fr>
2020-04-24 15:06:30 +0000

    add dropdown menu

---
+++ infos.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    
+
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores animi corrupti voluptas consectetur voluptatibus explicabo ex porro aspernatur impedit est dolor ducimus nam error, beatae labore vel culpa, natus saepe.</p>
+    <img src="" alt="">
+
+
+</body>
+</html>
--- templates/header.php
+++ templates/header.php
@@ -4,9 +4,15 @@
         <ul class="flex">
             <li class="flex"><a href="/">Accueil</a></li>
             <li class="flex"><a href="users.php">Utilisateurs</a></li>
+            <li class="flex"><a href="infos.php">Infos</a></li>
             <?php if (isset($_SESSION['name'])): ?>
-                <li class="flex"><p><?= ucfirst($_SESSION['name']) ?></p></li>
-                <li class="flex"><a href="controllers/logout.php">Déco</a></li>
+                <li class="drop">
+                    <span><?= ucfirst($_SESSION['name']) ?></span>
+                    <ul class="none">
+                        <li class="flex"><a href="#">Profil</a></li>
+                        <li class="flex"><a href="controllers/logout.php">Déco</a></li>
+                    </ul>    
+                </li>
             <?php else: ?>
                 <li class="flex"><a href="form.php">Connexion</a></li>
             <?php endif ?>
